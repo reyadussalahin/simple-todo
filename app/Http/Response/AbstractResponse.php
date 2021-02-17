10b48fe35dc5fd8572e02767dcda3d1590c4cfff
reyadussalahin <reyadussalahin@gmail.com>
2021-02-18 04:33:31 +0600

    Contracts for response objects
    Every response object mush implement ResponseInterface
    and provide sendResponse method. One may choose to
    extends AbstractResponse.

---
+++ app/Http/Response/AbstractResponse.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Response;
+
+use App\Contracts\Http\Response\ResponseInterface;
+
+
+abstract class AbstractResponse implements ResponseInterface
+{
+
+}
