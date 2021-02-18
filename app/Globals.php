ff1eab996bf6045c4ba1b115ae88161a61112204
reyadussalahin <reyadussalahin@gmail.com>
2021-02-18 18:10:38 +0600

    added test and comments for Globals
    added tests for App\Globals class in Tests\GlobalsTest
    and also added doc comments in App\Globals

--- app/Globals.php
+++ app/Globals.php
@@ -8,8 +8,19 @@ use App\Contracts\GlobalsInterface;
 
 class Globals implements GlobalsInterface
 {
+    /**
+     * Contains all the necessary globals
+     * 
+     * @var array
+     */
     private $globals;
 
+
+    /**
+     * Create a new Globals instance
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->globals = [
@@ -25,51 +36,101 @@ class Globals implements GlobalsInterface
         ];
     }
 
+    /**
+     * Returns globals array which consists of all globals
+     *
+     * @return array
+     */
     public function all()
     {
         return $this->globals;
     }
 
+    /**
+     * Returns PHP $GLOBALS array
+     *
+     * @return array
+     */
     public function &globals()
     {
         return $this->globals["globals"];
     }
 
+    /**
+     * Returns PHP $_SERVER array
+     *
+     * @return array
+     */
     public function &server()
     {
         return $this->globals["server"];
     }
 
+    /**
+     * Returns PHP $_GET array
+     *
+     * @return array
+     */
     public function &get()
     {
         return $this->globals["get"];
     }
 
+    /**
+     * Returns PHP $_POST array
+     *
+     * @return array
+     */
     public function &post()
     {
         return $this->globals["post"];
     }
 
+    /**
+     * Returns PHP $_FILES array
+     *
+     * @return array
+     */
     public function &files()
     {
         return $this->globals["files"];
     }
     
+    /**
+     * Returns PHP $_COOKIE array
+     *
+     * @return array
+     */
     public function &cookie()
     {
         return $this->globals["cookie"];
     }
 
+    /**
+     * Returns PHP $_SESSION array
+     *
+     * @return array
+     */
     public function &session()
     {
         return $this->globals["session"];
     }
 
+    /**
+     * Returns PHP $_REQUEST array
+     *
+     * @return array
+     */
     public function &request()
     {
         return $this->globals["request"];
     }
 
+    /**
+     * Returns PHP $_ENV array
+     *
+     * @return array
+     */
     public function &env()
     {
         return $this->globals["env"];
--- tests/GlobalsTest.php
+++ tests/GlobalsTest.php
@@ -14,4 +14,26 @@ class GlobalsTest extends TestCase
         $g = new Globals();
         $this->assertEquals(true, $g instanceof GlobalsInterface);
     }
+
+    public function testAll()
+    {
+        global $GLOBALS;
+        $GLOBALS["test-key"] = "test-val";
+        global $_SERVER;
+        $_SERVER["server-key"] = "server-val";
+
+        $g = new Globals();
+        $this->assertEquals($g->all()["globals"]["test-key"], "test-val");
+        $this->assertEquals($g->all()["server"]["server-key"], "server-val");
+    }
+
+    public function testSession()
+    {
+        global $_SESSION;
+        $_SESSION["key"] = "val";
+        $g = new Globals();
+        $this->assertEquals($g->session()["key"], "val");
+        $g->session()["another-key"] = "another-val";
+        $this->assertEquals($g->session()["another-key"], "another-val");
+    }
 }
