5c2010ca675a2ac3b74e900dfdb28f1d1ded39ec
reyadussalahin <reyadussalahin@gmail.com>
2021-02-18 18:12:11 +0600

    added comment in index.php
    described how index.php bootstraps and creates
    an application which handles request

--- public/index.php
+++ public/index.php
@@ -1,12 +1,29 @@
 <?php declare(strict_types=1);
 
+/**
+ * The unopinionated autoloader of php
+ * Loads everything we need and only when we need them
+ * We don't need to manage anything manually
+ */
 require __DIR__ . "/../vendor/autoload.php";
 
+
+/**
+ * Create Bootstrapper that would bootstrap the needs
+ * for creating an application
+ *
+ * And then create the App by passing the bootstrapper object
+ * Once the application is created, we can handle the request
+ */
 $app = new \App\App(
     new \App\Bootstrapper(
         dirname(__DIR__)
     )
 );
 
+/**
+ * Start process request
+ * and send response when done
+ */
 $app->processRequest()
     ->sendResponse();
